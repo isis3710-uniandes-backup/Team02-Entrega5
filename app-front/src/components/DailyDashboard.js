f7f07814e70a989796ae3d5d23b587473e1ed1b2
José Darío Flórez <jd.florezg1@uniandes.edu.co>
2019-12-08 17:21:57 -0500

    d3

--- app-front/src/components/DailyDashboard.js
+++ app-front/src/components/DailyDashboard.js
@@ -1,109 +1,174 @@
-import React, { Component } from 'react';
-import { Chart } from "react-google-charts";
+import React, { Component } from "react";
 import { data } from "../params";
-import { categories } from "../categories";
+import * as d3 from "d3";
+import { FormattedMessage } from "react-intl";
 const axios = require("axios");
+
 class DailyDashboard extends Component {
-    
-    state = {
-        chartData: ''
-    }
+  state = {
+    chartData: "",
+    chartData2: ""
+  };
 
-    componentDidMount() {
-        this.getChartData();
-    }
+  componentDidMount() {
+    this.getChartData();
+  }
 
-    getChartData = () => {
-        const fechaini = this.props.fechaIni;
-        const fechaFin = this.props.fechaFin;
-        //console.log(fechaini);
-        //console.log(fechaFin);
-        axios({
-            method: "POST",
-            url: data.getCostTime,
-            headers: { authorization: "Bearer " + localStorage.getItem("token") },
-            data:{
-                inicialTime:fechaini,
-                finalTime: fechaFin,
-            },
-        }).then(res => {
-            let categoriesData = {};
-            for (let c of categories) {
-                //console.log(c);
-                categoriesData[c] = 0;
-            }
-            //console.log(categoriesData);
-            //Calcular el total de cada categoria posible
-            for (let expense of res.data) {
-                //console.log(expense.amount);
-                categoriesData[expense.category] += parseInt(expense.amount);
-            }
-            //console.log(categoriesData);
-            this.setState({ chartData: categoriesData });
-            //console.log(this.state.chartData);
-        });
-    }
-
-    getData() {
-        let data = []
-        data.push(['Expenditure', 'Amount']);
-
-        for (let key in this.state.chartData) {
-            if (this.state.chartData[key] > 0) {
-                //console.log("Categoria SI tiene > 0");
-                data.push([key, this.state.chartData[key]]);
-            }
-        }
-        //console.log(data);
-        return data
-    }
-
-    render() {
-        if(this.props.username!== undefined){
+  graficar(){
+        var data1 = this.state.chartData;
+        console.log(data1);
+        var data2 = this.state.chartData2;
+        if(data1===''||data==={}){
             return (
-                <div className="container-float">
-                    <div className="card shadow chart-card">
-                        <div className="card-header text-center">
-                            <h3>Welcome back {this.props.username.charAt(0).toUpperCase() + this.props.username.slice(1)}!</h3>
-                        </div>
-                        <div className="card-body">
-                            <div className="row justify-content-center">
-                                <div className="col-12">
-                                    <Chart
-                                        width={'800px'}
-                                        height={'600px'}
-                                        chartType="PieChart"
-                                        loader={<div>Loading Chart...</div>}
-                                        data={this.getData()}
-                                        options={{
-                                            title: 'Your expenses',
-                                            // Just add this option
-                                            is3D: true,
-                                            backgroundColor: { fill: 'transparent' },
-                                            chartArea: {'width': '95%', 'height': '85%'},
-                                            titleTextStyle: {
-                                                fontSize: 30, // 12, 18 whatever you want (don't specify px)
-                                                bold: true,    // true or false
-    
-                                            }
-                                        }}
-                                        rootProps={{ 'data-testid': '1' }}
-                                    />
-                                </div>
-                            </div>
-    
-                        </div>
-    
-                    </div>
-                </div>
-            );
+                <h3>
+                  <FormattedMessage id="dashboard.noData" />
+                </h3>
+              );
         }
         else{
-            return(<div>No ha iniciado sesión</div>)
+
+        this.update(data1,data2);
         }
         
-    }
-}
+      }
 
+  update(data,data2) {
+    // set the dimensions and margins of the graph
+    var width = 450;
+    var height = 450;
+    var margin = 40;
+    // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+    var radius = Math.min(width, height) / 2 - margin;
+
+    // append the svg object to the div called 'my_dataviz'
+    var svg = d3
+      .select("#chart")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+
+    // set the color scale
+    var color = d3
+      .scaleOrdinal()
+      .domain(data)
+      .range(d3.schemeSet2);
+
+    // Compute the position of each group on the pie:
+    var pie = d3.pie().value(function(d) {
+      return d.value;
+    });
+    var data_ready = pie(d3.entries(data));
+    // Now I know that group A goes from 0 degrees to x degrees and so on.
+
+    // shape helper to build arcs:
+    var arcGenerator = d3
+      .arc()
+      .innerRadius(0)
+      .outerRadius(radius);
+
+    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+    svg
+      .selectAll("mySlices")
+      .data(data_ready)
+      .enter()
+      .append("path")
+      .attr("d", arcGenerator)
+      .attr("fill", function(d) {
+        return color(d.data.key);
+      })
+      .attr("stroke", "black")
+      .style("stroke-width", "2px")
+      .style("opacity", 0.7);
+
+    // Now add the annotation. Use the centroid method to get the best coordinates
+    svg
+      .selectAll("mySlices")
+      .data(data_ready)
+      .enter()
+      .append("text")
+      .text(function(d) {
+        return data2[d.data.key];
+      })
+      .attr("transform", function(d) {
+        return "translate(" + arcGenerator.centroid(d) + ")";
+      })
+      .style("text-anchor", "middle")
+      .style("font-size", 17);
+  }
+
+  getChartData = () => {
+    const fechaini = this.props.fechaIni;
+    const fechaFin = this.props.fechaFin;
+    //console.log(fechaini);
+    //console.log(fechaFin);
+    axios({
+      method: "POST",
+      url: data.getCostTime,
+      headers: { authorization: "Bearer " + localStorage.getItem("token") },
+      data: {
+        inicialTime: fechaini,
+        finalTime: fechaFin
+      }
+    }).then(res => {
+      let categoriesData = {};
+      let categoriesData2 = {};
+      //Calcular el total de cada categoria posible
+      for (let i=0;i<res.data.length;i++) {
+        //console.log(expense.amount);
+        categoriesData[i] = res.data[i].amount;
+        categoriesData2[i] = res.data[i].category;
+      }
+      //console.log(categoriesData);
+      this.setState({ chartData: categoriesData });
+      this.setState({ chartData2: categoriesData2 });
+      //console.log(this.state.chartData);
+    });
+  };
+
+  getData() {
+    let data = [];
+    data.push(["Expenditure", "Amount"]);
+
+    for (let key in this.state.chartData) {
+      if (this.state.chartData[key] > 0) {
+        //console.log("Categoria SI tiene > 0");
+        data.push([key, this.state.chartData[key]]);
+      }
+    }
+    //console.log(data);
+    return data;
+  }
+
+  render() {
+    if (this.props.username !== undefined) {
+      return (
+        <div className="container-float">
+          <div className="card shadow chart-card">
+            <div className="card-header text-center">
+              <h3>
+                Welcome back{" "}
+                {this.props.username.charAt(0).toUpperCase() +
+                  this.props.username.slice(1)}
+                !
+              </h3>
+            </div>
+            <div className="card-body">
+              <div className="row justify-content-center">
+                <div className="col-12">
+                <div id="chart">{this.graficar()}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return <div>No ha iniciado sesión</div>;
+    }
+  }
+}
 
 export default DailyDashboard;
